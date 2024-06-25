b23b4cbdce633232af6f50fc24704856dd88f540
MuriloTFROTB <pereiramurilo540@gmail.com>
2024-06-25 20:50:41 -0300

    fisrt commit

---
+++ android/app/src/main/kotlin/com/example/save_things/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.save_things
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:save_things/src/app_widget.dart';
+
+void main() {
+  runApp(
+    const AppWidget(),
+  );
+}
