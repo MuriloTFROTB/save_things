a3cf0f50542a5bb570b4c5eb957e8c61dc416efa
MuriloTFROTB <pereiramurilo540@gmail.com>
2024-06-26 19:14:00 -0300

    primeira versa da home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -11,10 +11,77 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'data',
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.only(top: 32.0, left: 8, right: 8),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  'Aqui você consegue visualizar os seus itens salvos',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.normal),
+                ),
+                Expanded(
+                  child: Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: [
+                      Container(
+                        width: (MediaQuery.of(context).size.width - 36) / 2,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+                        child: Column(
+                          children: [
+                            Image.network(
+                              'https://www.lojatsc.com.br/wp-content/uploads/2019/06/Funko-Pop-Stranger-Things-3-Max-806-800x800.png',
+                              width: double.infinity,
+                              height: 150,
+                              fit: BoxFit.cover,
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8),
+                              child: Text(
+                                'data',
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.black),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 200,
+                        width: (MediaQuery.of(context).size.width - 36) / 2,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Pesquisa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
           ),
         ],
       ),
