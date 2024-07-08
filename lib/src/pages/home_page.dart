a2d104ffded6e5d6f0e7106d1e8273c7f9aed92e
MuriloTFROTB <pereiramurilo540@gmail.com>
2024-07-08 17:48:33 -0300

    home_page2.0

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -15,14 +15,10 @@ class _HomePageState extends State<HomePage> {
         width: double.infinity,
         height: double.infinity,
         child: Padding(
-          padding: EdgeInsets.only(top: 32.0, left: 8, right: 8),
+          padding: const EdgeInsets.only(top: 32.0, left: 8, right: 8),
           child: Center(
             child: Column(
               children: [
-                Text(
-                  'Aqui você consegue visualizar os seus itens salvos',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.normal),
-                ),
                 Expanded(
                   child: Wrap(
                     spacing: 8,
@@ -34,31 +30,66 @@ class _HomePageState extends State<HomePage> {
                           borderRadius: BorderRadius.circular(10),
                           color: Colors.white,
                         ),
-                        child: Column(
-                          children: [
-                            Image.network(
-                              'https://www.lojatsc.com.br/wp-content/uploads/2019/06/Funko-Pop-Stranger-Things-3-Max-806-800x800.png',
-                              width: double.infinity,
-                              height: 150,
-                              fit: BoxFit.cover,
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(8),
-                              child: Text(
-                                'data',
-                                style: TextStyle(
-                                    fontSize: 18, color: Colors.black),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 160,
+                                child: Image.network(
+                                  'https://m.media-amazon.com/images/I/81pHqxTU7GS._AC_UF894,1000_QL80_.jpg',
+                                ),
                               ),
-                            ),
-                          ],
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Container(
+                                child: const Text(
+                                  'Funko Capitao AmericaFunko Capitao America',
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       Container(
-                        height: 200,
                         width: (MediaQuery.of(context).size.width - 36) / 2,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           color: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 160,
+                                child: Image.network(
+                                  'https://m.media-amazon.com/images/I/81pHqxTU7GS._AC_UF894,1000_QL80_.jpg',
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Container(
+                                child: const Text(
+                                  'Funko Capitao AmericaFunko Capitao America',
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ],
