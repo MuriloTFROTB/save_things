fb65fc68de47be29875065f27c5ecdf2a3957f1d
MuriloTFROTB <pereiramurilo540@gmail.com>
2024-07-10 18:02:04 -0300

    mexendo na pagina de adiconar itens

--- lib/src/pages/add_items.dart
+++ lib/src/pages/add_items.dart
@@ -10,6 +10,69 @@ class AddItens extends StatefulWidget {
 class _AddItensState extends State<AddItens> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              'Adicione aqui',
+            ),
+            TextField(
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                labelText: 'Nome',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            TextAreaExample()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextAreaExample extends StatefulWidget {
+  @override
+  _TextAreaExampleState createState() => _TextAreaExampleState();
+}
+
+class _TextAreaExampleState extends State<TextAreaExample> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 140,
+          child: TextField(
+            controller: _controller,
+            keyboardType: TextInputType.multiline,
+            maxLines: null, // Permite várias linhas
+            expands: true,
+            textAlignVertical: TextAlignVertical.top,
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              hintText: 'Digite seu texto aqui...',
+              border: OutlineInputBorder(),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:save_things/src/pages/add_items.dart';
+import 'package:save_things/src/pages/homePageContent.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,97 +10,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePageContent(),
+    AddItens(),
+    HomePageContent()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 32.0, left: 8, right: 8),
-          child: Center(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Wrap(
-                    spacing: 8,
-                    runSpacing: 8,
-                    children: [
-                      Container(
-                        width: (MediaQuery.of(context).size.width - 36) / 2,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 160,
-                                child: Image.network(
-                                  'https://m.media-amazon.com/images/I/81pHqxTU7GS._AC_UF894,1000_QL80_.jpg',
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Container(
-                                child: const Text(
-                                  'Funko Capitao AmericaFunko Capitao America',
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: (MediaQuery.of(context).size.width - 36) / 2,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 160,
-                                child: Image.network(
-                                  'https://m.media-amazon.com/images/I/81pHqxTU7GS._AC_UF894,1000_QL80_.jpg',
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Container(
-                                child: const Text(
-                                  'Funko Capitao AmericaFunko Capitao America',
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -107,14 +38,17 @@ class _HomePageState extends State<HomePage> {
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Pesquisa',
+            icon: Icon(Icons.add),
+            label: 'Adicionar',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: 'Perfil',
           ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber,
+        onTap: _onItemTapped,
       ),
     );
   }
